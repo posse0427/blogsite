7aa435f15fdd4ea2cc123966817c66c9970264cc
posse0427 <posse.kirby@gmail.com>
2020-02-13 00:33:47 +0900

    first commit to heroku

--- blogsite/settings/dev.py
+++ blogsite/settings/dev.py
@@ -1,11 +1,9 @@
 from .base import *
-import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-env = os.environ.copy()
-SECRET_KEY = env['SECRET_KEY']
+SECRET_KEY = '3xr*n=s20s=^2p)(n3fqo=1ymtl96kvd_23lfsokkc7jxepj@-'
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
