f1984d1b82161bfbb0e14c9b62840b9333bf8a73
posse0427 <posse.kirby@gmail.com>
2020-02-13 18:31:58 +0900

    first commit to heroku

--- blogsite/settings/dev.py
+++ blogsite/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SECRET_KEY = '3xr*n=s20s=^2p)(n3fqo=1ymtl96kvd_23lfsokkc7jxepj@-'
 
