9ffd3c2e268595adbf14afb0982ca28bb87cf5c0
posse0427 <posse.kirby@gmail.com>
2020-02-13 18:41:31 +0900

    first commit to heroku

--- blogsite/settings/production.py
+++ blogsite/settings/production.py
@@ -14,14 +14,6 @@ try:
 except ImportError:
     pass
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-COMPRESS_OFFLINE = True
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-COMPRESS_CSS_HASHING_METHOD = 'content'
 
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] = dj_database_url.config()
